62eb628a761e80d166560ba4fbf46991e023afcf
mpcdigitize <plawrow@hotmail.com>
2019-02-18 16:30:12 -0500

    added EF migrations for IsHidden property

---
+++ LogParser.Data/Migrations/201902110253322_change_ishidden_prop.cs
@@ -0,0 +1,16 @@
+namespace LogParser.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class change_ishidden_prop : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
