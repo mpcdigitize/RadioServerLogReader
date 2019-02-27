21d83dfc13ed46bf79cd5577335995a1eeb0d4d4
mpcdigitize <plawrow@hotmail.com>
2019-02-26 21:51:29 -0500

    add FileType static class

---
+++ LogParser.Domain/FileType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogParser.Domain
+{
+    public static class FileType
+    {
+        public static String Log { get { return ".log"; } }
+        public static String Txt { get { return ".txt"; } }
+        public static String Xml { get { return ".xml"; } }
+
+
+    }
+}
