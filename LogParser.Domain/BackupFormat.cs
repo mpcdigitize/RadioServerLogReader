e838b0f4b12cf2b26fe9f9d38a21ccc86ee64163
mpcdigitize <plawrow@hotmail.com>
2019-03-04 11:44:47 -0500

    add backup format class

---
+++ LogParser.Domain/BackupFormat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogParser.Domain
+{
+    public class BackupFormat
+    {
+         public static String Xml { get { return ".xml"; } }
+         public static String Json { get { return ".json"; } }
+    }
+}
