f81eddc03e7e9d82bda4bb8215a0e8f12556bf7f
mpcdigitize <plawrow@hotmail.com>
2019-03-17 16:48:50 -0400

    chnaged display names in IpDetailModel

--- LogParser.MVC/Models/IpDetailModel.cs
+++ LogParser.MVC/Models/IpDetailModel.cs
@@ -11,12 +11,19 @@ namespace LogParserMVC.Models
         [Display(Name = "IP Number")]
         [Required(ErrorMessage ="You neeed to give us Ip Number")]
         public string IpNumber { get; set; }
+
+
         public string IpDetailId { get; set; }
+
+        [Display(Name = "ISP Provider")]
         public string IspProvider { get; set; }
+
         public string Country { get; set; }
         public string Location { get; set; }
         public string State { get; set; }
         public string Alias { get; set; }
+
+        [Display(Name = "Hide")]
         public bool IsHidden { get; set; }
 
 
